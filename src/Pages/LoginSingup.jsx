3aae0e07aeda99875a586e8365576a9d4fcb6ee4
JabedHossainSwe <jabedhossain.swe@gmail.com>
2024-02-20 01:33:20 +0600

    Add API call to sign up a new user

--- src/Pages/LoginSingup.jsx
+++ src/Pages/LoginSingup.jsx
@@ -23,6 +23,24 @@ const LoginSingup = () => {
 
   const singup = async () => {
     console.log("Sign Up", formData);
+    let responseData;
+    await fetch("http://localhost:4000/signup", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => (responseData = data));
+
+    if (responseData.success) {
+      localStorage.setItem("auth-token", responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.errors);
+    }
   };
 
   return (
