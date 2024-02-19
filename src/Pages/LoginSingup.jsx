86922b2249492697f4112babae64d1b478fa56ef
JabedHossainSwe <jabedhossain.swe@gmail.com>
2024-02-20 01:01:52 +0600

    20 feb 24

--- src/Pages/LoginSingup.jsx
+++ src/Pages/LoginSingup.jsx
@@ -1,23 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CSS/LoginSignup.css";
 const LoginSingup = () => {
+  const [state, setState] = useState("Login");
+
   return (
     <div className="loginsingup">
       <div className="loginsingup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsingup-fields">
-          <input type="text" placeholder="Name"></input>
+          {state === "Sign Up" ? (
+            <input type="text" placeholder="Name"></input>
+          ) : (
+            <></>
+          )}
           <input type="email" placeholder="Email Address"></input>
           <input type="password" placeholder="Password"></input>
         </div>
         <button>Continue</button>
-        <p className="loginsingup-login">
-          Already have an account? <span>Login Here</span>
-          <div className="loginsingup-agree">
-            <input type="checkbox" name="" id=""></input>
-            <p>By Continuing, I agree to the terms of use & privacy policy.</p>
-          </div>
-        </p>
+        {state === "Sign Up" ? (
+          <p className="loginsingup-login">
+            Already have an account?{" "}
+            <span
+              onClick={() => {
+                setState("Login");
+              }}
+            >
+              Login Here
+            </span>
+          </p>
+        ) : (
+          <p className="loginsingup-login">
+            Create an Account{" "}
+            <span
+              onClick={() => {
+                setState("Sign Up");
+              }}
+            >
+              Click Here
+            </span>
+          </p>
+        )}
+
+        <div className="loginsingup-agree">
+          <input type="checkbox" name="" id=""></input>
+          <p>By Continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
       </div>
     </div>
   );
