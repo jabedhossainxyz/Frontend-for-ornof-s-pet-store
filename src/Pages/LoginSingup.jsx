1d9fc49f6c1eba54b881716e30ea0ab162abd81a
JabedHossainSwe <jabedhossain.swe@gmail.com>
2024-02-20 01:34:56 +0600

    Add login functionality (generated by blackbox.ai )

--- src/Pages/LoginSingup.jsx
+++ src/Pages/LoginSingup.jsx
@@ -19,6 +19,24 @@ const LoginSingup = () => {
 
   const login = async () => {
     console.log("Login", formData);
+    let responseData;
+    await fetch("http://localhost:4000/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => (responseData = data));
+
+    if (responseData.success) {
+      localStorage.setItem("auth-token", responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.errors);
+    }
   };
 
   const singup = async () => {
