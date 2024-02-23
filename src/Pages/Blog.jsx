7a56343d512b8f2a4f6cda8428f28463947c33cf
JabedHossainSwe <jabedhossain.swe@gmail.com>
2024-02-24 02:32:38 +0600

    Add blogs feature to the app

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./CSS/Blog.css";
+
 const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    // Fetch blogs from your backend API
+    fetch("http://localhost:4000/uploadedblogs")
+      .then((response) => response.json())
+      .then((data) => {
+        setBlogs(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching blogs:", error);
+      });
+  }, []);
+
   return (
     <div className="blog">
-      <h1>Blogs Page</h1>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/IG5cgtVJxjM?si=WeJYLZUJsWGTenWv"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+      <h1 className="blog-heading">Blogs Page</h1>
+      <div className="blog-container">
+        {blogs.map((blog) => (
+          <div key={blog._id} className="blog-item">
+            <h2 className="blog-title">{blog.title}</h2> 
+            <h4>{blog.author}</h4>
+            <iframe src={blog.videoLink} title={blog.title} />
+            <h5 className="video-link">
+              Video Link:{" "}
+              <a
+                href={blog.videoLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {blog.videoLink}
+              </a>{" "}
+            </h5>
+            <h6 className="blog-date">Upload Time: {blog.createdAt}</h6>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
